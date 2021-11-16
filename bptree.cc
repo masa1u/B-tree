abd38d53905fa09685d32e30e532a7f48c8e2478
masa1u <88305358+masa1u@users.noreply.github.com>
2021-11-16 15:39:10 +0900

    bptree_nonLeafSplit

--- bptree.cc
+++ bptree.cc
@@ -1,10 +1,19 @@
 #include "bptree.h"
 #include <vector>
 #include <sys/time.h>
+#include <random>
+
+#define DATA_SIZE 100
+
+struct list
+{
+	int data;
+	struct list *next;
+};
 
 NODE *split(NODE *leaf, int key, DATA *data);
 
-NODE *insert_in_parent(NODE *oldleaf, NODE *newleaf, int key, DATA *data);
+NODE *insert_in_parent(NODE *oldleaf, NODE *newleaf, int key);
 
 struct timeval
 cur_time(void)
@@ -16,20 +25,26 @@ cur_time(void)
 
 void print_tree_core(NODE *n)
 {
-	// printf("%d\n", Root->key[0]);
 	printf("[");
+	fflush(stdout);
 	for (int i = 0; i < n->nkey; i++)
 	{
+
 		if (!n->isLeaf)
 			print_tree_core(n->chi[i]);
 		printf("%d", n->key[i]);
+		fflush(stdout);
 
 		if (i != n->nkey - 1 && n->isLeaf)
+		{
 			putchar(' ');
+			fflush(stdout);
+		}
 	}
 	if (!n->isLeaf)
 		print_tree_core(n->chi[n->nkey]);
 	printf("]");
+	fflush(stdout);
 }
 
 void print_tree(NODE *node)
@@ -137,6 +152,48 @@ insert_in_root(NODE *root, int key, NODE *oldleaf, NODE *newleaf)
 }
 
 TEMP *
+insert_in_root_temp(TEMP *root, NODE *leftnode, NODE *rightnode, int key)
+{
+	int i;
+	// printf("%d", root->nkey);
+
+	if (key < root->key[0])
+	{
+		root->chi[N] = root->chi[N - 1];
+
+		for (i = root->nkey; i > 0; i--)
+		{
+			root->chi[i] = root->chi[i - 1];
+			root->key[i] = root->key[i - 1];
+		}
+
+		root->key[0] = key;
+		root->chi[0] = leftnode;
+		root->chi[1] = rightnode;
+	}
+	else
+	{
+		for (i = 0; i < root->nkey; i++)
+		{
+			if (key < root->key[i])
+				break;
+		}
+		root->chi[N] = root->chi[N - 1];
+		for (int j = root->nkey; j > i; j--)
+		{
+			root->chi[j] = root->chi[j - 1];
+			root->key[j] = root->key[j - 1];
+		}
+		root->key[i] = key;
+		root->chi[i] = leftnode;
+		root->chi[i + 1] = rightnode;
+	}
+	root->nkey++;
+
+	return root;
+}
+
+TEMP *
 insert_in_leaf_temp(TEMP *leaf, int key, DATA *data)
 {
 	int i;
@@ -198,42 +255,49 @@ copy(NODE *node)
 		temp->key[i] = node->key[i];
 		temp->chi[i] = node->chi[i];
 	}
+	temp->chi[N - 1] = node->chi[N - 1];
 	return temp;
 }
 
 NODE *
-split_root(NODE *leftroot, int newkey, DATA *data)
+split_root(NODE *leftnode, NODE *rightnode, NODE *upperleftnode, int newkey)
 {
 	TEMP *temp;
 	//0 copy
-	temp = copy(leftroot);
+
+	temp = copy(upperleftnode);
+	temp->isLeaf = false;
 	//1 insert
-	insert_in_leaf_temp(temp, newkey, data);
+	insert_in_root_temp(temp, leftnode, rightnode, newkey);
 
 	//2,3  pointer change
-	NODE *rightroot;
-	rightroot = alloc_leaf(leftroot->parent);
+	NODE *upperrightnode;
+	upperrightnode = alloc_leaf(upperleftnode->parent);
 
-	// leaf->chi[leaf->nkey] = (NODE *)data;
-	rightroot->chi[N - 1] = (NODE *)data;
+	upperrightnode->isLeaf = false;
 
-	leftroot->chi[N - 1] = rightroot;
+	// leftnode->chi[leftnode->nkey] = (NODE *)data;
+	// rightnode->chi[N - 1] = (NODE *)data;
+
+	// leftnode->chi[N - 1] = rightnode;
 
 	//4 clean up
-	for (int i = 0; i < rightroot->nkey; i++)
+	for (int i = 0; i < N - 1; i++)
 	{
-		leftroot->key[i] = 0;
-		leftroot->chi[i] = NULL;
+		upperleftnode->key[i] = 0;
+		upperleftnode->chi[i] = NULL;
 	}
-	leftroot->nkey = 0;
+	upperleftnode->chi[N - 1] = NULL;
+	upperleftnode->nkey = 0;
 
 	//5 copy to old leaf
 	for (int i = 0; i < N / 2; i++)
 	{
-		leftroot->key[i] = temp->key[i];
-		leftroot->chi[i] = temp->chi[i];
-		leftroot->nkey++;
+		upperleftnode->key[i] = temp->key[i];
+		upperleftnode->chi[i] = temp->chi[i];
+		upperleftnode->nkey++;
 	}
+	upperleftnode->chi[N / 2] = temp->chi[N / 2];
 
 	// 7
 	int key = temp->key[int(N / 2)];
@@ -241,101 +305,105 @@ split_root(NODE *leftroot, int newkey, DATA *data)
 	//6 copy to newleaf
 	for (int i = int(N / 2) + 1; i < temp->nkey; i++)
 	{
-		rightroot->key[i - int(N / 2) - 1] = temp->key[i];
-		rightroot->chi[i - int(N / 2) - 1] = temp->chi[i];
-		rightroot->nkey++;
+		upperrightnode->chi[i - int(N / 2) - 1] = temp->chi[i];
+		upperrightnode->key[i - int(N / 2) - 1] = temp->key[i];
+		upperrightnode->nkey++;
 	}
-	// printf("%d %d\n", leftroot->key[0], rightroot->key[0]);
+	upperrightnode->chi[upperrightnode->nkey] = temp->chi[temp->nkey];
 
 	//8 insert in parents
 
-	insert_in_parent(leftroot, rightroot, key, data);
+	insert_in_parent(upperleftnode, upperrightnode, key);
 
-	return leftroot;
+	//parent change
+	for (int i = 0; i <= upperrightnode->nkey; i++)
+	{
+		upperrightnode->chi[i]->parent = upperrightnode;
+	}
+
+	return leftnode;
 }
 
 NODE *
-insert_in_parent(NODE *oldleaf, NODE *newleaf, int key, DATA *data)
+insert_in_parent(NODE *leftnode, NODE *rightnode, int key)
 {
-	NODE *subRoot;
-	if (oldleaf->parent == NULL)
+	NODE *uppernode;
+	if (leftnode->parent == NULL)
 	{
+		uppernode = alloc_leaf(NULL);
+		Root = uppernode;
 
-		subRoot = alloc_leaf(NULL);
-		Root = subRoot;
-
-		subRoot->isLeaf = false;
+		uppernode->isLeaf = false;
 	}
 
 	else
 	{
-		subRoot = oldleaf->parent;
+		uppernode = leftnode->parent;
 	}
-	if (subRoot->nkey < (N - 1))
+	rightnode->parent = uppernode;
+	leftnode->parent = uppernode;
+	if (uppernode->nkey < (N - 1))
 	{
-		insert_in_root(subRoot, key, oldleaf, newleaf);
-		newleaf->parent = subRoot;
-		oldleaf->parent = subRoot;
+		insert_in_root(uppernode, key, leftnode, rightnode);
 	}
 	else
 	{
-		// split_root(subRoot, key, data);
-		newleaf->isLeaf = false;
-		oldleaf->isLeaf = false;
+		split_root(leftnode, rightnode, uppernode, key);
 	}
-	return subRoot;
+	return uppernode;
 }
 
 NODE *
-split(NODE *leaf, int newkey, DATA *data)
+split(NODE *leftleaf, int newkey, DATA *data)
 {
 	TEMP *temp;
 	//0 copy
-	temp = copy(leaf);
+	temp = copy(leftleaf);
 	//1 insert
 	insert_in_leaf_temp(temp, newkey, data);
 
 	//2,3  pointer change
-	NODE *newleaf;
-	newleaf = alloc_leaf(leaf->parent);
+	NODE *rightleaf;
+	rightleaf = alloc_leaf(leftleaf->parent);
 
-	// leaf->chi[leaf->nkey] = (NODE *)data;
-	newleaf->chi[N - 1] = (NODE *)data;
+	// leftleaf->chi[leftleaf->nkey] = (NODE *)data;
+	// rightleaf->chi[N - 1] = leftleaf->chi[N - 1];
+	rightleaf->chi[N - 1] = (NODE *)data;
 
-	leaf->chi[N - 1] = newleaf;
+	leftleaf->chi[N - 1] = rightleaf;
 
 	//4 clean up
-	for (int i = 0; i < leaf->nkey; i++)
+	for (int i = 0; i < N - 1; i++)
 	{
-		leaf->key[i] = 0;
-		leaf->chi[i] = NULL;
+		leftleaf->key[i] = 0;
+		leftleaf->chi[i] = NULL;
 	}
-	leaf->nkey = 0;
+	leftleaf->nkey = 0;
 
 	//5 copy to old leaf
 	for (int i = 0; i < N / 2; i++)
 	{
-		leaf->key[i] = temp->key[i];
-		leaf->chi[i] = temp->chi[i];
-		leaf->nkey++;
+		leftleaf->key[i] = temp->key[i];
+		leftleaf->chi[i] = temp->chi[i];
+		leftleaf->nkey++;
 	}
 
 	//6 copy to newleaf
 	for (int i = int(N / 2); i < temp->nkey; i++)
 	{
-		newleaf->key[i - int(N / 2)] = temp->key[i];
-		newleaf->chi[i - int(N / 2)] = temp->chi[i];
-		newleaf->nkey++;
+		rightleaf->key[i - int(N / 2)] = temp->key[i];
+		rightleaf->chi[i - int(N / 2)] = temp->chi[i];
+		rightleaf->nkey++;
 	}
 
 	//7
-	int key = newleaf->key[0];
+	int insertion_key = rightleaf->key[0];
 
 	//8 insert in parents
 
-	insert_in_parent(leaf, newleaf, key, data);
+	insert_in_parent(leftleaf, rightleaf, insertion_key);
 
-	return leaf;
+	return leftleaf;
 }
 
 void insert(int key, DATA *data)
@@ -378,19 +446,134 @@ int interactive()
 	return key;
 }
 
+struct list *ascending()
+{
+	struct list *temp_list, *start;
+	for (int i = 0; i < DATA_SIZE; i++)
+	{
+
+		if (i == 0)
+		{
+			temp_list = (list *)malloc(sizeof(struct list));
+			start = temp_list;
+		}
+		else
+		{
+			temp_list->next = (list *)malloc(sizeof(struct list));
+			temp_list = temp_list->next;
+		}
+
+		temp_list->data = i + 1;
+	}
+	temp_list->next = NULL;
+	return start;
+}
+
+struct list *descending()
+{
+	struct list *temp_list, *start;
+	for (int i = 0; i < DATA_SIZE; i++)
+	{
+
+		if (i == 0)
+		{
+			temp_list = (list *)malloc(sizeof(struct list));
+			start = temp_list;
+		}
+		else
+		{
+			temp_list->next = (list *)malloc(sizeof(struct list));
+			temp_list = temp_list->next;
+		}
+
+		temp_list->data = DATA_SIZE - i;
+	}
+	temp_list->next = NULL;
+	return start;
+}
+struct list *random_gene()
+{
+	struct list *temp_list, *start;
+	std::random_device rnd; // 非決定的な乱数生成器
+	std::mt19937 mt(rnd()); // メルセンヌ・ツイスタの32ビット版、引数は初期シード
+	std::uniform_int_distribution<> randDATA_SIZE(0, DATA_SIZE * 10);
+	for (int i = 0; i < DATA_SIZE; i++)
+	{
+
+		if (i == 0)
+		{
+			temp_list = (list *)malloc(sizeof(struct list));
+			start = temp_list;
+		}
+		else
+		{
+			temp_list->next = (list *)malloc(sizeof(struct list));
+			temp_list = temp_list->next;
+		}
+
+		temp_list->data = randDATA_SIZE(mt);
+	}
+	temp_list->next = NULL;
+	return start;
+}
+
 int main(int argc, char *argv[])
 {
 	struct timeval begin, end;
+	struct list *top;
 
 	init_root();
 
-	printf("-----Insert-----\n");
+	// printf("-----Insert-----\n");
 	begin = cur_time();
-	while (true)
+	// while (true)
+	// {
+	// 	insert(interactive(), NULL);
+	// 	print_tree(Root);
+	// }
+
+	//前から
+	// top = ascending();
+
+	//後ろから
+	// top = descending();
+
+	//ランダム
+	top = random_gene();
+
+	//insert
+	while (top)
 	{
-		insert(interactive(), NULL);
-		print_tree(Root);
+		insert(top->data, NULL);
+		top = top->next;
 	}
+
+	// print_tree(Root);
+	printf("build done\n");
+
+	//search
+
+	NODE *leaf;
+
+	while (top)
+	{
+		leaf = find_leaf(Root, top->data);
+		for (int i = 0; i < leaf->nkey; i++)
+		{
+			if (top->data == leaf->key[i])
+			{
+				// printf("found %d\n", top->data);
+				break;
+			}
+			else if (i == leaf->nkey)
+			{
+				printf("not found %d\n", top->data);
+			}
+		}
+	}
+
+	printf("search done\n");
+
 	end = cur_time();
 
 	return 0;
